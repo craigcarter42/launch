ed2694f63e584f55d6434fb4d73c6a1a73f59a88
craigcarter42 <craigcarter42@gmail.com>
2019-09-01 22:07:39 -0700

    1. Adding support for multiple sources. 2. More error handeling for missing sources, or broken applications. 3. Created method for checking args. 4. Added -sources arg to list out sources.

--- launch.py
+++ launch.py
@@ -12,9 +12,8 @@ except:
 #   Create shortcuts for applications
 #   Index/Re-Index/Delete Index options
 #   Autocompletion of application names
-#   oswalk
 
-INFO = '''-- launch v.1
+INFO = '''-- launch v.2
    Quickly index and launch applications in located in the /Applications folder
    in macOS. Applications nested within folders will not be found.
 
@@ -23,18 +22,43 @@ INFO = '''-- launch v.1
 
 class Launch:
     def __init__(self):
-        self.applications = []
-        self.source = '/Applications'
         self.index = {}
+        self.applications = []
+        self.keys = ['-help',
+                     '-list',
+                     '-sources'
+                     ]
+        self.sources = ['/Applications',
+                        '/Applications/Utilities'
+                       ]
 
-    def get_applications(self, count=0):
-        apps = os.listdir(self.source)
-        for app in apps:
-            if '.app' in app:
-                app = app.replace('.app', '')
-                self.applications.append(app)
-                self.index[app.lower()] = app
-                count = count + 1
+    def check_sources(self):
+        for source in self.sources:
+            if not os.path.exists(source):
+                print(' err: bad source given -> {}'.format(source))
+                exit()
+
+    def check_args(self, args):
+        if args == '-list':
+            self.list_applications(); exit()
+        elif args == '-help':
+            print(INFO); exit()
+        elif args == '-sources':
+            for source in self.sources:
+                print(' > {}'.format(source))
+            exit()
+
+    def get_applications(self, count=1):
+        for source in self.sources:
+            apps = os.listdir(source)
+            for app in apps:
+                if '.app' in app:
+                    app = app.replace('.app', '')
+                    self.applications.append(app)
+                    app_lower = app.lower()
+                    app_path = source + '/' + app
+                    self.index[app.lower()] = app_path
+                    count = count + 1
         return(self.applications)
         
     def list_applications(self, count=1):
@@ -43,24 +67,22 @@ class Launch:
             count = count + 1
 
     def launch_application(self, app):
-        cmd = "/usr/bin/open " + self.source + '/' + app
+        cmd = '/usr/bin/open {}'.format(app)
         out = os.system(cmd)
         if out != 0:
-            print('err: occured({})'.format(out))
+            print(' err: error ({})\n err: could not launch -> {}'.format(out, app))
 
 if __name__ == '__main__':
     launch = Launch()
+    launch.check_sources()
     launch.get_applications()
 
+# Check for and parse user input
     args = sys.argv
-    if len(args[1:]) == 0: print('try: --help'); quit()
+    if len(args[1:]) == 0: print(' try: --help'); quit()
     found_args = ' '.join(args[1:])
-    
-    if found_args == '--list':
-        launch.list_applications()
-    elif found_args == '--help':
-        print(INFO)
-        
+    launch.check_args(found_args)
+            
     found_args = found_args.lower()
     found_args = found_args.replace('.app', '')
     if found_args in launch.index:
@@ -68,3 +90,10 @@ if __name__ == '__main__':
         app = app.replace(' ', '\ ')
         app = app + '.app'
         launch.launch_application(app)
+    else:
+        print(' err: no application found -> {}'.format(found_args))
+
+
+
+
+
